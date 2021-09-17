c23140580f8cfcd32993431ad7d68be0b9614f2b
Stefan Sperling <stsp@stsp.name>
2021-09-17 12:18:45 +0200

    keep original licences for rlog.py, derived from viewvc and cvs2gitdump

--- swh/loader/cvs/rlog.py
+++ swh/loader/cvs/rlog.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2021  The Software Heritage developers
-# See the AUTHORS file at the top-level directory of this distribution
-# License: GNU General Public License version 3, or any later version
-# See top-level LICENSE file for more information
-
 """ RCS/CVS rlog parser, derived from viewvc and cvs2gitdump.py """
 
 # Copyright (C) 1999-2021 The ViewCVS Group. All Rights Reserved.
